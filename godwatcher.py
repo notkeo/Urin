c5a5b425f955d2a19e99085f3570532294d81e24
notkeo <notkeo@gmail.com>
2015-03-30 22:33:05 +0400

    refactor

--- godwatcher.py
+++ godwatcher.py
@@ -1,17 +1,20 @@
 import struct
 import binascii
 import logging
-from logging import debug, error
 from os import listdir
 from os.path import isfile, join
 from hashlib import md5
 
-logging.setLoggerClass(logging.DEBUG | logging.ERROR)
-# name of sample file in current directory
-sample_name = "TFTP.exe"
-# samples signature and hash values
-s_sig = 0
-s_hash = 0
+
+# log settings
+log = logging.getLogger("godwatcher-debug")
+log.addHandler(logging.FileHandler("./log/log.txt"))
+log.setLevel(logging.DEBUG)
+
+# params of current sample
+SAMPLE_FILE_NAME = "TFTP.exe"
+SAMPLE_SIG = 0
+SAMPLE_HASH = 0
 
 
 def hashcalc(file_path):
@@ -23,7 +26,7 @@ def hashcalc(file_path):
                 m.update(f.read())
                 return m.digest()
     except IOError:
-        error("access denied: %s" % file_path)
+        log.error("access denied: %s" % file_path)
 
 
 def extract_cs(file_path):
@@ -31,13 +34,13 @@ def extract_cs(file_path):
         with open(file_path, "rb") as f:
             if 'MZ' == f.read(2):
                 f.seek(0x3c)
-                e_lfanew = struct.unpack("<i", f.read(4))[0]  # offset to PE
-                f.seek(e_lfanew + 6)
-                sections_count = struct.unpack('H', f.read(2))[0]
-                debug("file: %s" % file_path)
-                debug("offset to PE section table %s" % binascii.hexlify(e_lfanew))
-                debug("sections count %d" % sections_count)
                 try:
+                    e_lfanew = struct.unpack("<i", f.read(4))[0]  # offset to PE
+                    f.seek(e_lfanew + 6)
+                    sections_count = struct.unpack('H', f.read(2))[0]
+                    log.debug("file: %s" % file_path)
+                    log.debug("offset to PE section table %s" % binascii.hexlify(str(e_lfanew)))
+                    log.debug("sections count %d" % sections_count)
                     for i in range(sections_count):  # loop sections
                         f.seek(e_lfanew + 248 + 40 * i)
                         section_name = f.read(8)
@@ -45,36 +48,47 @@ def extract_cs(file_path):
                             f.seek(e_lfanew + 248 + 40 * i + 16)
                             cs_prop = [struct.unpack('<i', f.read(4))[0], struct.unpack('<i', f.read(4))[0]]
                             f.seek(cs_prop[1])
-                            debug(".text size: %s , offset: %s" % (cs_prop[0], cs_prop[1]))
+                            log.debug(".text size: %s , offset: %s" % (cs_prop[0], cs_prop[1]))
                             return f.read(cs_prop[0])
                 except struct.error:
-                    error("MZ file was packed: %s , e_lfanew: " % (file_path, binascii.hexlify(e_lfanew)))
+                    log.error(
+                        "MZ file was packed: %s , e_lfanew: %s" % (file_path, binascii.hexlify(str(e_lfanew))))
+
     except IOError:
-        error("access denied: %s" % file_path)
+        log.error("access denied: %s" % file_path)
 
 
 props = eval(open("props.txt", 'r').readline())
 if props['type'] == 'sig':
-    sample_cs = extract_cs(sample_name)
-    s_sig = sample_cs[len(sample_cs) / 2:len(sample_cs) / 2 + 32]
-    print "sample sig: ", binascii.hexlify(s_sig)
+    sample_cs = extract_cs(SAMPLE_FILE_NAME)
+    SAMPLE_SIG = sample_cs[len(sample_cs) / 2:len(sample_cs) / 2 + 32]
+    print "sample sig: ", binascii.hexlify(SAMPLE_SIG)
 else:
-    s_hash = hashcalc(sample_name)
-    print 'sample md5: ', binascii.hexlify(s_hash)
+    SAMPLE_HASH = hashcalc(SAMPLE_FILE_NAME)
+    print 'sample md5: ', binascii.hexlify(SAMPLE_HASH)
 
 dirs_queue = [props['path']]
 while len(dirs_queue) != 0:
     current_dir = dirs_queue.pop()
-    entries = listdir(current_dir)
-    for e in entries:
-        path = join(current_dir, e)
-        if isfile(path):
-            if props['type'] == 'sig':
-                cs = extract_cs(path)
-                if cs is not None and s_sig in cs:
-                    print "[***OK***] catched by sig: ", path.encode("utf-8")
+    try:
+        entries = listdir(current_dir)
+        for e in entries:
+            path = join(current_dir, e)
+            if isfile(path):
+                if path.lower().endswith('.exe'):
+                    if props['type'] == 'sig':
+                        cs = extract_cs(path)
+                        if cs is not None and SAMPLE_SIG in cs:
+                            print "[***OK***] catched by sig: ", path.encode("utf-8")
+                    else:
+                        if SAMPLE_HASH == hashcalc(path):
+                            print "[***OK***] catched by hash: ", path.encode("utf-8")
             else:
-                if s_hash == hashcalc(path):
-                    print "[***OK***] catched by hash: ", path.encode("utf-8")
-        else:
-            dirs_queue.append(path)
+                dirs_queue.append(path)
+    except WindowsError:
+        log.error("access denied %s", current_dir)
+
+
+
+
+
